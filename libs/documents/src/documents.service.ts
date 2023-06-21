8f6c6b49966c0605880dae466f3a2176caba6d1e
杭麟 <hanglin@kalendsoft.com>
2023-06-21 21:34:20 +0800

    chore: 删除错误字段

--- libs/documents/src/documents.service.ts
+++ libs/documents/src/documents.service.ts
@@ -36,7 +36,6 @@ export class DocumentsService {
           // eslint-disable-next-line @typescript-eslint/no-var-requires
           require('mkdirp').sync(mkdir)
         }
-        s
         const type = getFileType(decodeURIComponent(escape(file.originalname)))
         const pathSrc = join(mkdir, `${new Date().getTime()}.${type}`)
         const writeImage = createWriteStream(pathSrc)
